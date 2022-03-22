181c60a9f3109cdfe37f7f09526dc610e311486d
Choi-Ju12g <choi-ju12g@naver.com>
2022-03-22 23:40:44 +0900

    crawl.js success

--- crawl.js
+++ crawl.js
@@ -23,7 +23,7 @@ const crawl = async () => {
     await page.goto('http://portal.ndhs.or.kr/index');
 
     await page.click('body > div > div > div > div > div > div.row > div > div.login-body > div > div.col-xs-12.col-sm-5.login-con.pt20 > div > form > ul > li:nth-child(2) > a');
-    await page.waitForTimeout('1000');
+    //await page.waitForTimeout('1000');
     //아이디랑 비밀번호 란에 값을 넣어라
     await page.evaluate((id, pw) => {
     document.querySelector('#stuUserId').value = id;
@@ -40,8 +40,20 @@ const crawl = async () => {
     //로그인 성공 시(화면 전환 성공 시)
     if(page.url() === 'http://portal.ndhs.or.kr/dashboard/dashboard'){
         //학사 페이지로 가서
-        await page.goto('http://portal.ndhs.or.kr/studentLifeSupport/carte/list')
-        
+        await page.goto('http://portal.ndhs.or.kr/studentLifeSupport/carte/list');
+        const content = await page.content();
+        const $ = cheerio.load(content);
+        const lists = $("body > div.container-fluid > div:nth-child(6) > div > table > tbody > tr");
+        lists.each((index, list) => {
+            MealList[index] = {
+                date: $(list).find("th").text().replace('\n\t\t\t\t\t\t\t\t',""),
+                breakfast:$(list).find("td:nth-of-type(1)").text(),
+                lunch:$(list).find("td:nth-of-type(2)").text(),
+                dinner:$(list).find("td:nth-of-type(3)").text()
+            }
+            //const texts = $(list).find("th").text();
+            console.log(MealList[index]);
+        })
           
     }
     //로그인 실패시

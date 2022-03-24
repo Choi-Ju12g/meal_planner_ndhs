42ef2716abbec75a198980709c801f6f86315524
Choi-Ju12g <choi-ju12g@naver.com>
2022-03-24 20:57:24 +0900

    set mobile view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,19 @@ function App() {
   return (
     <div className="App">
       <LoginForm></LoginForm>
-      <button type="button" onClick={() => {
-        fetch('/api/data')
-        .then((res) => {
-          return res.json();
-        })
-        .then((data) => {
-          console.log(data);
-        });
-      }}>get data</button>
     </div>
   );
 }
 
 export default App;
+
+
+// <button type="button" onClick={() => {
+//         fetch('/api/data')
+//         .then((res) => {
+//           return res.json();
+//         })
+//         .then((data) => {
+//           console.log(data);
+//         });
+//       }}>get data</button>
--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -7,53 +7,42 @@ import pass from "../statics/images/pass.png";
 
 export function LoginForm(){
 
+    return(
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile"/>
+            </div>
+          </div>
+        <div>
+          <h1>Login Page</h1>
+          <div>
+            <img src={email} alt="email" className="email"/>
+            <input type="text" placeholder="남도학숙 아이디" className="name"/>
+          </div>
+          <div className="second-input">
+            <img src={pass} alt="pass" className="email"/>
+            <input type="password" placeholder="남도학숙 비밀번호" className="name"/>
+          </div>
+          <div className="login-button">
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  );
+}
+
+
     // return (
     // <LoginContainer className='login_container'>
     //   <InputForm placeholder='남도학숙 아이디를 적어주세요'></InputForm>
     //   <InputForm placeholder='남도학숙 비밀번호를 적어주세요'></InputForm>
     // </LoginContainer>
     // )
-
-    return(
-        <div className="main">
-     <div className="sub-main">
-       <div>
-         <div className="imgs">
-           <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
-
-           </div>
-
-
-         </div>
-         <div>
-           <h1>Login Page</h1>
-           <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
-           </div>
-           <div className="second-input">
-             <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
-           </div>
-          <div className="login-button">
-          <button>Login</button>
-          </div>
-           
-            <p className="link">
-              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
-            </p>
-           
- 
-         </div>
-       </div>
-       
-
-     </div>
-    </div>
-  );
-}
-
 // const LoginContainer = styled.div`
 //     @media screen and (max-width: 500px) {
 //         position: relative;

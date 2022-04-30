cd36c515719b14290fdfee507217cb5539f537c4
Choi-Ju12g <choi-ju12g@naver.com>
2022-04-30 10:44:49 +0900

    로그인 페이지 스타일드 컴포넌트 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navigation from "Navigation";
 import {LoginForm} from "./components/LoginForm";
-import { MenuPlan } from "components/MenuPlan";
+import { MenuPlan } from "components/MenuPlan"
+import { LoginPage } from 'components/LoginPage';
 
 function App() : JSX.Element {
   return (
         <Routes>
 
-          <Route path="/"  element={<LoginForm/>}></Route>
+          <Route path="/"  element={<LoginPage/>}></Route>
           <Route path="/menuplan" element={<MenuPlan/>}></Route>
         </Routes>
   );
--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,49 +1,104 @@
-import { useState } from 'react';
-import '../statics/css/LoginForm.css';
 import profile from "../statics/images/a.png";
 import email from "../statics/images/email.jpg";
 import pass from "../statics/images/pass.png";
+import styled from 'styled-components';
 
-export function LoginForm() {
-  const [inputID, setInputID] = useState('');
-  const [inputPW, setInputPW] = useState('');
-
-  function onCLicklogin(): void {
-
-      fetch('/api/data')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  }
+export function LoginPage() {
 
   return (
-    <div className="main">
-      <div className="sub-main">
+    <MainWrapper className="main">
+      <SubWrapper className="sub-main">
         <div>
-          <div className="imgs">
-            <div className="container-image">
-              <img src={profile} alt="profile" className="profile" />
-            </div>
-          </div>
+          <Imgs className="imgs">
+            <ImgsContainer className="container-image">
+              <Profile src={profile} alt="profile" className="profile" />
+            </ImgsContainer>
+          </Imgs>
           <div>
             <h1>Login Page</h1>
             <div>
-              <img src={email} alt="email" className="email" />
-              <input type="text" placeholder="남도학숙 아이디" className="name" />
+              <Email src={email} alt="email" className="email" />
+              <InputForm type="text" placeholder="남도학숙 아이디" className="name" />
             </div>
-            <div className="second-input">
-              <img src={pass} alt="pass" className="email" />
-              <input type="password" placeholder="남도학숙 비밀번호" className="name" />
+            <div className="second-input" style={{paddingTop: '25px'}}>
+              <Email src={pass} alt="pass" className="email" />
+              <InputForm type="password" placeholder="남도학숙 비밀번호" className="name" />
             </div>
-            <div className="login-button">
-              <button type='button'>Login</button>
+            <div className="login-button" style={{paddingTop: '25px'}}>
+              <Button type='button'>Login</Button>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </SubWrapper>
+    </MainWrapper>
   );
 }
+
+const MainWrapper = styled.div`
+  text-align: center;
+  justify-content: center;
+  display: flex; 
+  padding-top: 10vh;
+  padding-bottom: 10vh;
+  background-color: #e1e3eb;
+`
+
+const SubWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 80vh;
+  width: 85vw;
+  box-shadow: 11px 12px 13px 12px rgb(207, 207, 207);
+  padding-top: 5vh;
+  border-radius: 60px; 
+  background-color: white;
+`
+
+const Imgs = styled.div`
+  justify-content: center;
+  display: flex;
+  padding-top: 5vh;
+`
+
+const ImgsContainer = styled.div`
+    background-color: rgb(223, 221, 221); 
+border-radius: 150px;
+align-items: center;
+display: flex;
+justify-content: center;
+height: 115px;
+width: 115px;
+`
+
+const Profile = styled.img`
+    height: 100px;
+width: 100px;
+border-radius: 130px;
+`
+const Email = styled.img`
+    height: 25px;
+  width: 25px;
+  position: absolute;
+  padding: 14px 0 0 25px;
+`
+const InputForm = styled.input`
+    width: 60vw;
+  height: 7vh;
+  border-radius: 60px;
+  box-shadow: inset 0px 0px 25px 0px #888;
+  border: none;
+  outline: none;
+  background-color: #fff;
+  padding-left: 70px;
+  font-size: 20px;
+`
+
+const Button = styled.button`
+    width: 80vw;
+  height: 5vh;
+  border-radius: 60px;
+  background-color: #b103fc;
+  color: white;
+  font-size: 25px;
+  border: none;
+`
